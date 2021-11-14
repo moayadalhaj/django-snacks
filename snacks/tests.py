2e6dc16ca17e38ce361de4783f3455759efd4faa
moayadalhaj <moayad.alhaj21@gmail.com>
2021-11-14 17:04:04 +0200

    feat: create first django project and app with two pages

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
-
+from django.http import response
+from django.test import SimpleTestCase
+from django.urls import reverse
 # Create your tests here.
+
+
+class SnacksTests(SimpleTestCase):
+    def test_home_page_status_code(self):
+        url = reverse("home")
+        response=self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_home_page_template(self):
+        url = reverse("home")
+        response=self.client.get(url)
+        self.assertTemplateUsed(response,"home.html")  
+    
+    def test_about_us_page_status_code(self):
+        url = reverse('about_us')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_about_us_template(self):
+        url = reverse('about_us')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, "about_us.html")
---
+++ snacks/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (
+                    HomeView,
+                    AboutUsView
+                )
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('about-us', AboutUsView.as_view(), name='about_us')
+]
